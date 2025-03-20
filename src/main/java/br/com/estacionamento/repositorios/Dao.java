9d59652824a3f94e69f499a3c566cb0162784597
bobonimo111 <carlosmaycon443@gmail.com>
2025-03-20 17:53:13 -0300

    Corrigindo mudanças

---
+++ src/main/java/br/com/estacionamento/repositorios/Dao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.estacionamento.repositorios;
+
+import java.util.List;
+
+/**
+ *
+ * @author william.rodrigues
+ */
+public interface Dao<T> {
+    public void adicionar(T  t);
+
+}
--- src/main/java/br/com/estacionamento/repositorios/impl/ClienteRepositorio.java
+++
@@ -1,4 +0,0 @@
-package br.com.estacionamento.repositorios.impl;
-
-public class ClienteRepositorio {
-}
