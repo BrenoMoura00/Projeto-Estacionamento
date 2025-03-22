7fa1a9bb1a91c570a9a350372921273790c3ab29
William Rodrigues <86368140+Bobonimo111@users.noreply.github.com>
2025-03-22 16:22:16 -0300

    Adicionado os métodos básicos de crud.java

--- src/main/java/br/com/estacionamento/repositorios/Dao.java
+++ src/main/java/br/com/estacionamento/repositorios/Dao.java
@@ -11,6 +11,19 @@ import java.util.List;
  * @author william.rodrigues
  */
 public interface Dao<T> {
-    public void adicionar(T  t);
 
+    // adiciona um obejto ao banco de dados
+    void adicionar(T  t);
+
+    // Lista todos os itens do banco de dados
+    List<T> listar();
+
+    // Busca um objeto em especifico no banco de dados
+    // T buscarUm(int id);
+    
+    // Atualiza um item no banco de dados;
+    // T update(T t);
+    
+    // Remove um item do banco de dados
+    // T remove(T t);
 }
