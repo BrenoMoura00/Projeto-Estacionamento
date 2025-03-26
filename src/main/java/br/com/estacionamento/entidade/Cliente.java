0f48b54544e58aa253a4182ebde61daadbc37dcb
William Rodrigues <Carlosmaycon443@gmail.com>
2025-03-26 10:43:29 -0300

    Adicioanando Builder Classico

--- src/main/java/br/com/estacionamento/entidade/Cliente.java
+++ src/main/java/br/com/estacionamento/entidade/Cliente.java
@@ -2,12 +2,7 @@ package br.com.estacionamento.entidade;
 
 import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-@Builder
+
 public class Cliente {
     private String nome;
     private String cpf;
@@ -15,4 +10,44 @@ public class Cliente {
     private int idade;
     private Endereco endereco;
 
+    public Cliente(Builder builder){
+        this.nome = builder.nome;
+        this.cpf = builder.cpf;
+        this.telefone = builder.telefone;
+        this.idade = builder.idade;
+        this.endereco = builder.endereco;
+    }
+
+    public static class Builder{
+        private String nome;
+        private String cpf;
+        private String telefone;
+        private int idade;
+        private Endereco endereco;
+
+        public Builder nome(String nome){
+            this.nome = nome;
+            return this;
+        }
+        public Builder cpf(String cpf){
+            this.cpf = cpf;
+            return this;
+        }
+        public Builder telefone(String telefone){
+            this.telefone = telefone;
+            return this;
+        }
+        public Builder idade(int idade){
+            this.idade = idade;
+            return this;
+        }
+        public Builder endereco(Endereco endereco){
+            this.endereco = endereco;
+            return this;
+        }
+
+        public Cliente build(){
+            return new Cliente(this);
+        }
+    }
 }
