b2bff161f526643ddce83d9bb295bc20fa194a3e
bobonimo <carlosmaycon443@gmail.com>
2025-05-15 22:24:44 -0300

    Realizada conexão com banco de dados, falta corrigir erros de legacy

---
+++ src/main/java/br/com/estacionamento/entities/ClienteModel.java
@@ -0,0 +1,47 @@
+package br.com.estacionamento.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
