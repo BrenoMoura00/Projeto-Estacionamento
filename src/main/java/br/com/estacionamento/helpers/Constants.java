913f8803a523b0ba71c70c3ae46388fe9530489b
Breno Moura <133166425+BrenoMoura00@users.noreply.github.com>
2025-05-25 14:22:16 -0300

    Deletando arquivos não nescessários

--- src/main/java/br/com/estacionamento/helpers/Constants.java
+++
@@ -1,5 +0,0 @@
-package br.com.estacionamento.helpers;
-
-public class Constants {
-    public static final String URL_DATABASE = "jdbc:mysql://localhost:3306/Estacionamento?user=admin&password=admin123";
-}
